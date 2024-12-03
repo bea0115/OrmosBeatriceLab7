37a989476c53fc016f36a129c3d85e2ed45836ff
Beatrice Ormos <madalinaormos@yahoo.com>
2024-12-04 00:05:48 +0200

    laborator 9- complet

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -6,7 +6,7 @@ public partial class ListPage : ContentPage
 {
 	public ListPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
@@ -20,6 +20,37 @@ public partial class ListPage : ContentPage
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        var selectedItem = listView.SelectedItem as Product;
+        if (selectedItem != null)
+        {
+            await App.Database.DeleteProductAsync(selectedItem);
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(((ShopList)BindingContext).ID);
+        }
+        else
+        {
+            await DisplayAlert("Error", "Please select an item to delete.", "OK");
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
     }
 
 }
